e36b72c45b69aab2821becd6a2da91083c1812ab
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-03 00:58:56 -0500

    made corrections to my views/styles.css file, renamed jquery.js file, and added styling to css

--- server/routes/todo.routuer.js
+++ server/routes/todo.routuer.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const koalaRouter = express.Router();
+const todoRouter = express.Router();
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+const pg = require('pg');
+
+//add bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('server/public'));
+
+
+
+
 // Routes go here
-app.listen(5000, function () {
-   console.log(`You started the server! It is running on port 5000.`);
-})
-data.js (optional)
-let data = [];
-module.exports = data;
+
+
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+  });
+
+
