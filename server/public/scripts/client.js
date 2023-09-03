4d6fa8b2f81b401de55b4176814017a9ade2b2d2
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-03 14:53:32 -0500

    created assTask function and created in put values, updated click listener on submit button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,41 @@ $(document).ready(onReady);
 function onReady() {
    console.log("Client side javascript works!");
 }
+//set up click listeners
+setupClickListeners()
+//load to do list on the application to load
+
+
+function setupClickListeners() {
+    $('#addSubmitButton').on('click', addTask);
+    $('#viewToDoList').on('click', 'markAsCompleteButton', putTask);
+
+}
+// set up function to and create input values
+function addTask (){
+    console.log('addSubmitButton on click');
+
+    let newTask = $('#taskIn').val();
+    let newPriority = $('#priorityIn').val();
+    let newStatus =  $('#statusyIn').val();
+    let newNotes = $('#NotesIn').val();
+    
+    //make new object to send to the server
+    let taskToSend = {
+        task: newTask, 
+        priority: newPriority,
+        status: newStatus, // must be true or false
+        notes: newNotes
+
+    };
+    //call saveTask with new object
+    postTask(taskToSend);
+    function postTask(newTask){
+        console.log('in POST/task', newTask)
+    }
+
+    
+
+}
+
+function putTask(){}
